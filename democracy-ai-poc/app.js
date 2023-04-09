05be00e7e98429ef0d802380bdafcb5ee93b49a5
Charles Thompson <charlesthompson1121@gmail.com>
2023-04-09 11:34:43 +0300

    Added Proof Of Concept

---
+++ democracy-ai-poc/app.js
@@ -0,0 +1,22 @@
+document.getElementById('summarize').addEventListener('click', async () => {
+    const legislation = document.getElementById('legislation').value;
+    const summaryElement = document.getElementById('summary');
+  
+    summaryElement.textContent = 'Summarizing...';
+  
+    try {
+      const summary = await generateSummary(legislation);
+      summaryElement.textContent = summary;
+    } catch (error) {
+      summaryElement.textContent = 'Error: Unable to generate summary.';
+      console.error(error);
+    }
+  });
+  
+  async function generateSummary(legislation) {
+    const apiKey = 'API-Key';
+    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type':
+  
