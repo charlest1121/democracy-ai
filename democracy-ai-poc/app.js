f443ef01cf70c1e3b0179154deff165bd774eccc
Charles Thompson <charlesthompson1121@gmail.com>
2023-04-09 14:44:00 +0300

    Added missing code

--- democracy-ai-poc/app.js
+++ democracy-ai-poc/app.js
@@ -1,22 +1,39 @@
 document.getElementById('summarize').addEventListener('click', async () => {
-    const legislation = document.getElementById('legislation').value;
-    const summaryElement = document.getElementById('summary');
-  
-    summaryElement.textContent = 'Summarizing...';
-  
-    try {
-      const summary = await generateSummary(legislation);
-      summaryElement.textContent = summary;
-    } catch (error) {
-      summaryElement.textContent = 'Error: Unable to generate summary.';
-      console.error(error);
-    }
+  const legislation = document.getElementById('legislation').value;
+  const summaryElement = document.getElementById('summary');
+
+  summaryElement.textContent = 'Summarizing...';
+
+  try {
+    const summary = await generateSummary(legislation);
+    summaryElement.textContent = summary;
+  } catch (error) {
+    summaryElement.textContent = 'Error: Unable to generate summary.';
+    console.error(error);
+  }
+});
+
+async function generateSummary(legislation) {
+  const apiKey = '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';
+  const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      prompt: `Summarize the following legislation: ${legislation}`,
+      max_tokens: 50,
+      n: 1,
+      stop: null,
+      temperature: 0.7,
+    }),
   });
-  
-  async function generateSummary(legislation) {
-    const apiKey = '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';
-    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type':
-  
+
+  if (!response.ok) {
+    throw new Error('Failed to generate summary');
+  }
+
+  const data = await response.json();
+  return data.choices[0].text.trim();
+}
